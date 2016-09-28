1493640fa9da3ae2d07c53aef14a6e172ee3c86c
Juho Vähä-Herttua <juhovh@iki.fi>
2016-09-28 21:18:51 +0300

    First version of the driver, still work in progress.

---
+++ esp_uart.c
@@ -0,0 +1,296 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Juho Vähä-Herttua (juhovh@iki.fi)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#include "esp_uart.h"
+
+#include "ets_sys.h"
+#include "uart_register.h"
+
+#define UART0 0
+#define UART1 1
+
+// Missing defines from SDK
+#define FUNC_U0RXD 0
+
+// Define FIFO sizes, actually 128 but playing safe
+#define UART_RXFIFO_SIZE 126
+#define UART_TXFIFO_SIZE 126
+#define UART_RXTOUT_TH   2
+#define UART_RXFIFO_TH   100
+#define UART_TXFIFO_TH   10
+
+// Define some helper macros to handle FIFO functions
+#define UART_TXFIFO_LEN(uart_no) \
+  ((READ_PERI_REG(UART_STATUS(uart_no)) >> UART_TXFIFO_CNT_S) & UART_RXFIFO_CNT)
+#define UART_TXFIFO_PUT(uart_no, byte) \
+  WRITE_PERI_REG(UART_FIFO(uart_no), (byte) & 0xff)
+#define UART_RXFIFO_LEN(uart_no) \
+  ((READ_PERI_REG(UART_STATUS(uart_no)) >> UART_RXFIFO_CNT_S) & UART_RXFIFO_CNT)
+#define UART_RXFIFO_GET(uart_no) \
+  READ_PERI_REG(UART_FIFO(uart_no))
+
+
+#define RINGBUF_SIZE 512
+struct ringbuf_s {
+  uint8 data[RINGBUF_SIZE];
+  uint16 head;
+  uint16 len;
+};
+typedef struct ringbuf_s ringbuf_t;
+
+#define RINGBUF_CLEAR(buf) ((buf)->head = (buf)->len = 0)
+#define RINGBUF_TAIL(buf) (((buf)->head + (buf)->len) % RINGBUF_SIZE)
+#define RINGBUF_PUT(buf, byte)\
+  ((buf)->len < RINGBUF_SIZE ? \
+    (buf)->data[RINGBUF_TAIL(buf)] = (byte), (sint32) ++(buf)->len : \
+    (sint32) -1)
+#define RINGBUF_GET(buf) \
+  ((buf)->len > 0 ? \
+    (buf)->len--, (sint32) (buf)->data[(buf)->head++ % RINGBUF_SIZE] : \
+    (sint32) -1)
+
+
+ringbuf_t uart0_rx_ringbuf;
+ringbuf_t uart0_tx_ringbuf;
+
+static uint16
+uart0_rxfifo_move(ringbuf_t *dst, uint16 nbytes)
+{
+  uint16 i;
+
+  for (i=0; i<nbytes; i++) {
+    if (dst->len >= RINGBUF_SIZE) {
+      break;
+    }
+    RINGBUF_PUT(dst, UART_RXFIFO_GET(UART0));
+  }
+
+  return i;
+}
+
+static void
+uart0_intr_handler(void *arg)
+{
+  uint32 uart0_status;
+
+  uart0_status = READ_PERI_REG(UART_INT_ST(UART0));
+  if (uart0_status & UART_RXFIFO_TOUT_INT_ST) {
+    uint16 uart0_rxfifo_len;
+
+    // No character received for some time, read to ringbuf
+    WRITE_PERI_REG(UART_INT_CLR(UART0), UART_RXFIFO_TOUT_INT_ST);
+
+    uart0_rxfifo_len = UART_RXFIFO_LEN(UART0);
+    uart0_rxfifo_move(&uart0_rx_ringbuf, uart0_rxfifo_len);
+  } else if (uart0_status & UART_RXFIFO_FULL_INT_ST) {
+    uint16 uart0_rxfifo_len;
+
+    // RX buffer becoming full, read to ringbuf
+    WRITE_PERI_REG(UART_INT_CLR(UART0), UART_RXFIFO_FULL_INT_ST);
+
+    uart0_rxfifo_len = UART_RXFIFO_LEN(UART0);
+    uart0_rxfifo_move(&uart0_rx_ringbuf, uart0_rxfifo_len);
+  } else if (uart0_status & UART_TXFIFO_EMPTY_INT_ST) {
+    uint16 i;
+
+    // TX buffer empty, check if ringbuf has space or disable int
+    WRITE_PERI_REG(UART_INT_CLR(UART0), UART_TXFIFO_EMPTY_INT_ST);
+
+    for (i=UART_TXFIFO_LEN(UART0); i<UART_TXFIFO_SIZE; i++) {
+      if (uart0_tx_ringbuf.len == 0) {
+        CLEAR_PERI_REG_MASK(UART_INT_ENA(UART0), UART_TXFIFO_EMPTY_INT_ENA);
+        break;
+      }
+      UART_TXFIFO_PUT(UART0, RINGBUF_GET(&uart0_tx_ringbuf));
+    }
+  }
+}
+
+void ICACHE_FLASH_ATTR
+uart0_open(uint32 baud_rate, uint32 flags)
+{
+  uint32 clkdiv;
+
+  ETS_UART_INTR_DISABLE();
+
+  // Set both RX and TX pins to correct mode
+  PIN_FUNC_SELECT(PERIPHS_IO_MUX_U0TXD_U, FUNC_U0TXD);
+  PIN_FUNC_SELECT(PERIPHS_IO_MUX_U0RXD_U, FUNC_U0RXD);
+
+  // Disable pullup on TX pin, enable on RX pin
+  PIN_PULLUP_DIS(PERIPHS_IO_MUX_U0TXD_U);
+  PIN_PULLUP_EN(PERIPHS_IO_MUX_U0RXD_U);
+
+  // Configure baud rate for the port
+  clkdiv = (UART_CLK_FREQ / baud_rate) & UART_CLKDIV_CNT;
+  WRITE_PERI_REG(UART_CLKDIV(UART0), clkdiv);
+
+  // Configure parameters for the port
+  WRITE_PERI_REG(UART_CONF0(UART0), flags);
+
+  // Reset UART0
+  uart0_reset(baud_rate, flags);
+}
+
+void ICACHE_FLASH_ATTR
+uart0_reset()
+{
+  // Disable interrupts while resetting UART0
+  ETS_UART_INTR_DISABLE();
+
+  // Clear all RX and TX buffers and flags
+  SET_PERI_REG_MASK(UART_CONF0(UART0), UART_RXFIFO_RST | UART_TXFIFO_RST);
+  CLEAR_PERI_REG_MASK(UART_CONF0(UART0), UART_RXFIFO_RST | UART_TXFIFO_RST);
+  RINGBUF_CLEAR(&uart0_rx_ringbuf);
+  RINGBUF_CLEAR(&uart0_tx_ringbuf);
+
+  // Set RX and TX interrupt thresholds
+  WRITE_PERI_REG(UART_CONF1(UART0),
+    UART_RX_TOUT_EN |
+    ((UART_RXTOUT_TH & UART_RX_TOUT_THRHD) << UART_RX_TOUT_THRHD_S) |
+    ((UART_RXFIFO_TH & UART_RXFIFO_FULL_THRHD) << UART_RXFIFO_FULL_THRHD_S) |
+    ((UART_TXFIFO_TH & UART_TXFIFO_EMPTY_THRHD) << UART_TXFIFO_EMPTY_THRHD_S));
+
+  // Disable all existing interrupts and enable ours
+  WRITE_PERI_REG(UART_INT_CLR(UART0), 0xffff);
+  SET_PERI_REG_MASK(UART_INT_ENA(UART0), UART_RXFIFO_TOUT_INT_ENA);
+  SET_PERI_REG_MASK(UART_INT_ENA(UART0), UART_RXFIFO_FULL_INT_ENA);
+
+  // Restart the interrupt handler for UART0
+  ETS_UART_INTR_ATTACH(uart0_intr_handler, NULL);
+  ETS_UART_INTR_ENABLE();
+}
+
+uint16 ICACHE_FLASH_ATTR
+uart0_available()
+{
+  uint16 ret;
+
+  ETS_UART_INTR_DISABLE();
+  ret = uart0_rx_ringbuf.len;
+  ETS_UART_INTR_ENABLE();
+
+  return ret;
+}
+
+uint16 ICACHE_FLASH_ATTR
+uart0_available_wait()
+{
+  uint16 uart0_rxfifo_len;
+  uint16 ret;
+
+  ETS_UART_INTR_DISABLE();
+  uart0_rxfifo_len = UART_RXFIFO_LEN(UART0);
+  if (uart0_rx_ringbuf.len == 0 && uart0_rxfifo_len == 0) {
+    ETS_UART_INTR_ENABLE();
+    while ((uart0_rxfifo_len = UART_RXFIFO_LEN(UART0)) == 0);
+    ETS_UART_INTR_DISABLE();
+  }
+  if (uart0_rx_ringbuf.len == 0 && uart0_rxfifo_len > 0) {
+    uart0_rxfifo_move(&uart0_rx_ringbuf, uart0_rxfifo_len);
+  }
+  ret = uart0_rx_ringbuf.len;
+  ETS_UART_INTR_ENABLE();
+
+  return ret;
+}
+
+uint16 ICACHE_FLASH_ATTR
+uart0_read_buf(const void *buf, uint16 nbytes)
+{
+  uint8 *data = buf;
+  uint16 i;
+
+  ETS_UART_INTR_DISABLE();
+  for (i=0; i<nbytes; i++) {
+    if (uart0_rx_ringbuf.len == 0) break;
+    data[i] = RINGBUF_GET(&uart0_rx_ringbuf);
+  }
+  ETS_UART_INTR_ENABLE();
+
+  return i;
+}
+
+uint16 ICACHE_FLASH_ATTR
+uart0_write_buf(const void *buf, uint16 nbytes)
+{
+  uint8 *data = buf;
+  uint16 i;
+
+  ETS_UART_INTR_DISABLE();
+  for (i=0; i<nbytes; i++) {
+    if (uart0_tx_ringbuf.len == RINGBUF_SIZE) break;
+    RINGBUF_PUT(&uart0_tx_ringbuf, data[i]);
+  }
+  SET_PERI_REG_MASK(UART_INT_ENA(UART0), UART_TXFIFO_EMPTY_INT_ENA);
+  ETS_UART_INTR_ENABLE();
+
+  return i;
+}
+
+void ICACHE_FLASH_ATTR
+uart0_flush()
+{
+  while (UART_TXFIFO_LEN(UART0));
+}
+
+void ICACHE_FLASH_ATTR
+uart1_open(uint32 baud_rate, uint32 flags)
+{
+  uint32 clkdiv;
+
+  ETS_UART_INTR_DISABLE();
+
+  // Set TX pin to correct mode
+  PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO2_U, FUNC_U1TXD_BK);
+
+  // Disable pullup on TX pin
+  PIN_PULLUP_DIS(PERIPHS_IO_MUX_U0TXD_U);
+
+  // Configure baud rate for the port
+  clkdiv = (UART_CLK_FREQ / baud_rate) & UART_CLKDIV_CNT;
+  WRITE_PERI_REG(UART_CLKDIV(UART1), clkdiv);
+
+  // Configure parameters for the port
+  WRITE_PERI_REG(UART_CONF0(UART1), flags);
+
+  // Reset UART1
+  uart1_reset(baud_rate, flags);
+
+  ETS_UART_INTR_ENABLE();
+}
+
+void ICACHE_FLASH_ATTR
+uart1_reset(uint32 baud_rate, uint32 flags)
+{
+  // Clear all TX buffers
+  SET_PERI_REG_MASK(UART_CONF0(UART1), UART_TXFIFO_RST);
+  CLEAR_PERI_REG_MASK(UART_CONF0(UART1), UART_TXFIFO_RST);
+}
+
+uint8 ICACHE_FLASH_ATTR
+uart1_write_byte(uint8 byte)
+{
+  UART_TXFIFO_PUT(UART1, byte);
+  while (UART_TXFIFO_LEN(UART1));
+}
---
+++ esp_uart.h
@@ -0,0 +1,72 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Juho Vähä-Herttua (juhovh@iki.fi)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifndef ESP_UART_H
+#define ESP_UART_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <c_types.h>
+
+#define UART_STOP_BITS_ONE      0x10
+#define UART_STOP_BITS_ONE_HALF 0x20
+#define UART_STOP_BITS_TWO      0x30
+#define UART_STOP_BITS_MASK     0x30
+
+#define UART_BITS_FIVE          0x00
+#define UART_BITS_SIX           0x04
+#define UART_BITS_SEVEN         0x08
+#define UART_BITS_EIGHT         0x0C
+#define UART_BITS_MASK          0x0C
+
+#define UART_PARITY_EVEN        0x00
+#define UART_PARITY_ODD         0x01
+#define UART_PARITY_NONE        0x02
+#define UART_PARITY_MASK        0x03
+
+#define UART_FLAGS_8N1 (UART_BITS_EIGHT | UART_PARITY_NONE | UART_STOP_BITS_ONE)
+
+#define UART_STATUS_IDLE        0x00
+#define UART_STATUS_RECEIVING   0x01
+#define UART_STATUS_OVERFLOW    0x02
+
+void uart0_open(uint32 baud_rate, uint32 flags);
+void uart0_reset();
+
+uint16 uart0_available();
+uint16 uart0_available_wait();
+uint16 uart0_read_buf(const void *buf, uint16 nbyte);
+uint16 uart0_write_buf(const void *buf, uint16 nbyte);
+void uart0_flush();
+
+void uart1_open(uint32 baud_rate, uint32 flags);
+void uart1_reset();
+uint8 uart1_write_byte(uint8 byte);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
