2f566c560e807924c712b2041d41d6bf207af858
René Koppe <rene@xargon.de>
2018-07-26 13:25:59 +0200

    Fix parity bits

--- esp_uart.h
+++ esp_uart.h
@@ -43,7 +43,7 @@ extern "C" {
 
 #define UART_PARITY_EVEN        0x00
 #define UART_PARITY_ODD         0x01
-#define UART_PARITY_NONE        0x02
+#define UART_PARITY_NONE        0x00
 #define UART_PARITY_MASK        0x03
 
 #define UART_FLAGS_8N1 (UART_BITS_EIGHT | UART_PARITY_NONE | UART_STOP_BITS_ONE)
